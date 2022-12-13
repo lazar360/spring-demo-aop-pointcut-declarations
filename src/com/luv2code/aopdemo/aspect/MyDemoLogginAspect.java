87fd6fe37fdd792b58950a60f6a9f5dd97b54465
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2022-12-13 20:45:43 +0100

    update src/com/luv2code/aopdemo/aspect/MyDemoLogginAspect.java

--- src/com/luv2code/aopdemo/aspect/MyDemoLogginAspect.java
+++ src/com/luv2code/aopdemo/aspect/MyDemoLogginAspect.java
@@ -2,6 +2,7 @@ package com.luv2code.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
@@ -11,44 +12,19 @@ public class MyDemoLogginAspect {
 	// this is where we add all of our related advices for logging 
 	
 	// let's start with an @Before advice
+			
+	@Pointcut("execution(* com.luv2code.aopdemo.dao.*.*(..))")
+	private void forDaoPackage(){};
 	
-	//@Before("execution(public void addAccount())")
-	
-	
-	//L'avertissement ne vise qu'une classe
-	//@Before("execution(public void com.luv2code.aopdemo.dao.AccountDAO.addAccount())")
-	
-	// Vise toutes les méthodes commençant par add
-	//@Before("execution(public void add*())")
-	
-	// Le modifier est facultatif donc on le retire
-//	@Before("execution(* add*())")
-//	public void beforeAddAccountAdvice() {
-//		
-//		System.out.println("\n ======>>> Executing @Before advice on addAccount()");
-//	}
-	
-	//On applique l'expression point cut à une classe d'objet
-	//Pour plusieurs paramètre on ajoute : ",.."
-//	@Before("execution(* add*(com.luv2code.aopdemo.Account, ..))")
-//	public void beforeAddAccountAdvice() {
-//		
-//		System.out.println("\n ======>>> Executing @Before advice on addAccount()");
-//	}
-	
-	
-	// Correspond à tous les paramètres
-//		@Before("execution(* add*(..))")
-//		public void beforeAddAccountAdvice() {
-//			
-//			System.out.println("\n ======>>> Executing @Before advice on addAccount()");
-//		}
-		
-	// Ne vise que le package avant-deernière étoile pour toutes les classes et dernière étoile pour toutes les méthodes
-	@Before("execution(* com.luv2code.aopdemo.dao.*.*(..))")
+	@Before("forDaoPackage()")
 	public void beforeAddAccountAdvice() {
-		
 		System.out.println("\n ======>>> Executing @Before advice on addAccount()");
+	}
+	
+	@Before("forDaoPackage()")
+	public void performApiAnalytics() {
+		System.out.println("\n ======>>> Performing API analytics");
+			
 	}
 	
 }
